240fdbe8f6ba147e889bf5f65241494369cda540
Cerberos4238 <105981834+Cerberos4238@users.noreply.github.com>
2024-05-16 14:39:24 +0200

    Update VPN_update.ps1

--- VPN_update.ps1
+++ VPN_update.ps1
@@ -1,17 +1,19 @@
-$fichier = "fortiClientVPNOnlineInstaller.exe"
+$file = "fortiClientVPNOnlineInstaller.exe"
+
+
 
 if (-not (Test-Path "C:\temp")) {
     New-Item "C:\temp" -ItemType Directory
 }
 
-Write-Host "Mise a jour du VPN" -ForegroundColor Yellow
+Write-Host "VPN Update`n`n" -ForegroundColor Yellow
 
-Write-Host "Telechargement du paquet en cours..." -ForegroundColor Green
-Invoke-WebRequest -Uri "https://links.fortinet.com/forticlient/win/vpnagent" -OutFile "C:\temp\$fichier"
+Write-Host "Downloading the latest package..." -ForegroundColor Green
+Invoke-WebRequest -Uri "https://links.fortinet.com/forticlient/win/vpnagent" -OutFile "C:\temp\$file"
 
-Write-Host "Installation de la mise a jour..." -ForegroundColor Green
-Start-Process C:\temp\$fichier -ArgumentList "/s" -wait
+Write-Host "Installation..." -ForegroundColor Green
+Start-Process C:\temp\$file -ArgumentList "/s" -wait
 
 Stop-Process -Name "fortiClientVPNOnlineInstaller.exe"
 Start-Process C:\temp\FortiClientVPN.exe -ArgumentList /quiet -wait
-Remove-Item -Path "C:\temp\$fichier" -Force -Recurse
+Remove-Item -Path "C:\temp\$file" -Force -Recurse
