6a3fda1306d096a88ca400b4b36878fb22d7947e
Cerberos4238 <105981834+Cerberos4238@users.noreply.github.com>
2024-05-17 15:50:06 +0200

    FortiVPN_Auto.ps1

--- VPN_update.ps1
+++ VPN_update.ps1
@@ -1,19 +1,44 @@
 $file = "fortiClientVPNOnlineInstaller.exe"
-
+$DwlPath = "C:\temp\$file"
+$vpnexec = "C:\Program Files\Fortinet\FortiClient\FortiClient.exe"
 
 
 if (-not (Test-Path "C:\temp")) {
     New-Item "C:\temp" -ItemType Directory
 }
 
-Write-Host "VPN Update`n`n" -ForegroundColor Yellow
+Write-Host "VPN Update`n" -ForegroundColor Yellow
 
-Write-Host "Downloading the latest package..." -ForegroundColor Green
-Invoke-WebRequest -Uri "https://links.fortinet.com/forticlient/win/vpnagent" -OutFile "C:\temp\$file"
+try {
+    Write-Host "Downloading the latest package..." -ForegroundColor Green
+    Invoke-WebRequest -Uri "https://links.fortinet.com/forticlient/win/vpnagent" -OutFile $DwlPath -ErrorAction SilentlyContinue
 
-Write-Host "Installation..." -ForegroundColor Green
-Start-Process C:\temp\$file -ArgumentList "/s" -wait
+    Write-Host "Installation...`n" -ForegroundColor Green
 
-Stop-Process -Name "fortiClientVPNOnlineInstaller.exe"
-Start-Process C:\temp\FortiClientVPN.exe -ArgumentList /quiet -wait
-Remove-Item -Path "C:\temp\$file" -Force -Recurse
+if (-not (Test-Path $vpnexec)) {
+    try {
+        Start-Process $DwlPath -ArgumentList /quiet -wait -ErrorAction SilentlyContinue
+        Write-Host "Installation Complete`n" -ForegroundColor Green
+        Remove-Item -Path $DwlPath -Force -Recurse -ErrorAction SilentlyContinue
+        exit
+    } catch {
+            Write-Host "An error has occured. The program has to close prematurely`n" -ForegroundColor Red
+            Start-Sleep -Seconds 1
+            Write-Host "Goodbye!" -ForegroundColor Yellow
+            Remove-Item -Path $DwlPath -Force -Recurse -ErrorAction SilentlyContinue
+            throw
+    }
+} else {
+        Write-Host "Vpn is already installed. No installation needed`n" -ForegroundColor Yellow 
+        Start-Sleep -Seconds 1
+        Write-Host "Goodbye!" -ForegroundColor Yellow
+        Remove-Item -Path $DwlPath -Force -Recurse -ErrorAction SilentlyContinue
+        exit
+}} 
+catch {
+        Write-Host "An error has occured. The program has to close prematurely`n" -ForegroundColor Red
+        Start-Sleep -Seconds 1
+        Write-Host "Goodbye!" -ForegroundColor Yellow
+        Remove-Item -Path $DwlPath -Force -Recurse -ErrorAction SilentlyContinue
+        exit
+}
